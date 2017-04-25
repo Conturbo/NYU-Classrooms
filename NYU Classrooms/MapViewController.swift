a73d45df6a5a22246fde6dfcc8d6e29c5d149711
matthewtessler <matthewtessler@172-16-209-44.DYNAPOOL.NYU.EDU>
2017-04-25 12:16:42 -0400

    filler map displays nyc

--- NYU Classrooms/MapViewController.swift
+++ NYU Classrooms/MapViewController.swift
@@ -23,30 +23,14 @@ class MapViewController: UIViewController {
     }
     
     
-    
-    
-    
-    /* EXAMPLE CODE SHOWS A MAP BUT ONLY THE PIN, NO DETAIL */
-    // You don't need to modify the default init(nibName:bundle:) method.
-    
+    // NYC is currently displayed. Eventually this will be sent the address.
+    // The marker will be at that location, and the address will be displayed in the title and snippet.
     override func loadView() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude:40.7128 , longitude:-74.0059, zoom: 12.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
     }
-    
-     /* EXAMPLE CODE SHOWS A MAP BUT ONLY THE PIN, NO DETAIL */
-    
-    
     
 
     /*
