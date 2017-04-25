145aa2cd27bc81dc5a6e873ce02b35f8b92c23d7
matthewtessler <matthewtessler96@gmail.com>
2017-04-25 11:52:57 -0400

    api key fix now shows map detail

--- NYU Classrooms/AppDelegate.swift
+++ NYU Classrooms/AppDelegate.swift
@@ -19,14 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        if let path = Bundle.main.path(forResource: "Property_List", ofType: "plist"), let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
-            // use swift dictionary as normal
-            
-            let s = String(describing: dict["api_key"])
-            GMSServices.provideAPIKey(s)
-        }
-       
-        
+        GMSServices.provideAPIKey(apiKey)
+    
         return true
     }
 
