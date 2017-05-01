81ef80114cb6807d01d4511fd21bfd3ac71c9d29
Conturbo <conturbodev@gmail.com>
2017-05-01 11:25:45 -0400

    stashing changes

---
+++ Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  NYU Classrooms
+//
+//  Created by Matthew Tessler on 4/25/17.
+//  Copyright © 2017 Conturbo Dev. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "AIzaSyAyGfYaY_LExbVBWBEtVUzre_uD3Zz6mYU"
+let gcAPIKey = "AIzaSyBg1897reJ9iMMzZi6fxxH2MDsOEHBi5OI"
